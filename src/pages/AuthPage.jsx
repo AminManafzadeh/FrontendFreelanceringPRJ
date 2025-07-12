9fee25f88ee4cadf0dc92e0a025f66e8fcc60389
AminManafzadeh <amin.mah1375@gmail.com>
2025-07-12 00:53:44 -0700

    add some changes

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -3,8 +3,10 @@ import AuthContainer from "../features/authentication/AuthContainer";
 function AuthPage() {
   return (
     <div className=" bg-secondary-0 h-screen">
-      <div className=" flex items-center justify-center pt-16">
-        <AuthContainer />
+      <div className="container xl:max-w-screen-xl">
+        <div className=" flex items-center justify-center pt-16">
+          <AuthContainer />
+        </div>
       </div>
     </div>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,9 @@
 function HomePage() {
   return (
     <div className=" bg-secondary-0 h-screen text-secondary-900">
-      <div>HomePage</div>
+      <div className="container xl:max-w-screen-xl pt-6">
+        <div>صفحه اصلی</div>
+      </div>
     </div>
   );
 }
